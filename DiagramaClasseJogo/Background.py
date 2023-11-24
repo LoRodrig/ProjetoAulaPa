02af193728e813958a3aa0fe4210fe92c2767919
Lorenzo.r <rodrigueslorenzo092@gmail.com>
2023-11-24 11:42:37 -0300

    Signed-off-by: Lorenzo.r <rodrigueslorenzo092@gmail.com>

---
+++ DiagramaClasseJogo/Background.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Background import Background
+from Entity1 import Entity1
+
+class Background(Background, Entity1):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Entity1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity1:
+    def __init__(self):
+        self.nome: str = None
+        self.sur: Surface = None
+        self.rect: Rect = None
+
+    def move(self, ):
+        pass
+
